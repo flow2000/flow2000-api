fe7870aa562819d7683906674e50f099c0052600
flow2000 <1982989137@qq.com>
2022-12-22 22:45:23 +0800

    60s接口的解析器由lxml改为html.parser

--- sixty_api/sixty_api.py
+++ sixty_api/sixty_api.py
@@ -19,7 +19,7 @@ def get_topic(offset=0):
     try:
         data=(requests.get(API+str(offset), headers=headers).json())
         html = data['data'][0]['content']
-        soup = BeautifulSoup(html, 'lxml')
+        soup = BeautifulSoup(html, 'html.parser')
         day_news = soup.find_all('p')
         for i in range(len(day_news)):
             text=day_news[i].get_text()
