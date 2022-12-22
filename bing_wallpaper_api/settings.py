d3a413605dccf59271e092b0e78b0a3ef15bf96b
flow2000 <1982989137@qq.com>
2022-12-22 23:09:53 +0800

    更新版本号

--- bing_wallpaper_api/settings.py
+++ bing_wallpaper_api/settings.py
@@ -14,7 +14,7 @@ DEPLOY_TYPE = "github" # 可选 github docker
 ##############################项目固定配置，请勿修改################################
 
 # 版本号
-VERSION="3.2.1"
+VERSION="1.0.0"
 
 # 必应接口
 BINGAPI='https://bing.com/HPImageArchive.aspx?n=1&format=js&idx=0'
